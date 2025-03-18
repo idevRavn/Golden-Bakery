2b0cc120827b7b067d36ac4a80d87d4425759164
Sanu Kumar <idevravn@gmail.com>
2025-03-18 19:25:09 +0530

    add tab switch function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import "./styles.css";
+import home from "./pages/home.js";
+import menu from "./pages/menu.js";
+import contact from "./pages/contact.js";
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    switch (e.target.id) {
+      case "home":
+        home();
+        break;
+      case "menu":
+        menu();
+        break;
+      case "contact":
+        contact();
+        break;
+      default:
+        home();
+    }
+  });
+});
+
+home();
